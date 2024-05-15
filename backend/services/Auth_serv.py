3b91b72d58350dc1fd6cbc4f6d60eea8a3a864ff
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-05-15 20:49:14 +0100

    test

--- backend/services/Auth_serv.py
+++ backend/services/Auth_serv.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 import sys
 import os
+
+# Get the absolute path of the parent directory
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Add the parent directory to sys.path
+sys.path.append(parent_dir)
+# Convert sys.path to a set to remove duplicates, then convert it back to a list
+unique_sys_path = list(set(sys.path))
+
+# Print the unique entries in sys.path
+for path in unique_sys_path:
+    print(path)
 import bcrypt
 import jwt
 import datetime
